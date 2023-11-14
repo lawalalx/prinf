8429d679e2b5ede123aa6c3bcdd38f314b59b7b9
lawalalx <lawal.alx@gmail.com>
2023-11-13 22:19:07 -0800

    test

--- main.h
+++ main.h
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int printf_char(va_list var);
@@ -16,5 +17,6 @@ int printf_string(va_list var);
 int print_37(void);
 int print_dec(va_list args);
 int print_int(va_list args);
+
 
 #endif /* MAIN_H */
