7e0a623b6f01be17c2c48ecb52723b3d520d4b0d
Olanrewaju <144465715+lawalalx@users.noreply.github.com>
2023-11-15 03:33:48 +0000

    Update main.h

--- main.h
+++ main.h
@@ -24,6 +24,7 @@ typedef struct format
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int printf_HEX(va_list val);
 int printf_char(va_list var);
 int _strlen(char *s);
 int _strlenc(const char *s);
