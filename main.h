0ae72633ee569f188b2b53dc22fc5d309afba1b9
Olanrewaju <144465715+lawalalx@users.noreply.github.com>
2023-11-15 03:39:38 +0000

    Update main.h

--- main.h
+++ main.h
@@ -27,6 +27,7 @@ int _putchar(char c);
 int printf_HEX(va_list val);
 int printf_HEX_aux(unsigned int num);
 int printf_bin(va_list val);
+int printf_exclusive_string(va_list val);
 int printf_char(va_list var);
 int _strlen(char *s);
 int _strlenc(const char *s);
