61605651452db50841711381073eb27650d1ea1b
lawalalx <lawal.alx@gmail.com>
2023-11-14 20:19:00 -0800

    test

--- printf.c
+++ printf.c
@@ -12,9 +12,18 @@ int _printf(const char *format, ...)
 	match m[] = {
 		{"%c", printf_char},
 		{"%s", printf_string},
-		{"%%", print_37},
-		{"%d", print_dec},
-		{"%i", print_int}
+		{"%%", printf_37},
+		{"%d", printf_dec},
+		{"%i", printf_int},
+		{"%r", printf_srev},
+		{"%R", printf_rot13},
+		{"%b", printf_bin},
+		{"%u", printf_unsigned},
+		{"%o", printf_oct},
+		{"%x", printf_hex},
+		{"%X", printf_HEX},
+		{"%S", printf_exclusive_string},
+		{"%p", printf_pointer}
 	};
 
 	va_list args;
@@ -22,8 +31,7 @@ int _printf(const char *format, ...)
 	int j;
 
 	va_start(args, format);
-
-	if (format == NULL || format[0] == '%' && format[1] == '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 Here:
